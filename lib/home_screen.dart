ef5a40f5c130ed4fd1a2ef45a1c5f6f3ea63a94e
TsalistAgna <tsalistagna29@gmail.com>
2025-05-21 10:50:05 +0800

    bottombar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'task_with_timer.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,6 +8,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.deepPurple,
+        onTap: (index){
+          // Handle button tap
+          if (index == 0) {
+            // Navigate to home screen
+          } else if (index == 1) {
+            // Navigate to add task screen
+          } else if (index == 2) {
+            // Navigate to profile screen
+          }
+        },
+        items: [
+        Icon(
+          Icons.home,
+          color: Colors.white,
+          ),
+        Icon(
+          Icons.add,
+          color: Colors.white,
+          ),
+        Icon(
+          Icons.person,
+          color: Colors.white,
+          ),
+      ]
+      ),
+
       appBar: AppBar(
         title: const Text('HatchBit'),
         backgroundColor: Colors.purple.shade200, // opsional, bisa diganti sesuai tema kamu

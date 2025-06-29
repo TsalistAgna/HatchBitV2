d9299ce6db5552abdf91e0ddfc33ffb9a5db34ee
F1D022012_Michael Effendy <143796680+OrionSeeker@users.noreply.github.com>
2025-06-29 17:37:10 +0800

    change icon app

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,12 +1,9 @@
-// main_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-// Import ketiga halaman utama Anda
 import 'home_screen.dart';
-import 'addTask.dart'; // File ini berisi kelas AddHabitPage
-import 'profile.dart';   // File ini berisi kelas ProfileScreen
+import 'addTask.dart';
+import 'profile.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -16,33 +13,25 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  // Variabel untuk melacak halaman yang sedang aktif
   int _pageIndex = 0;
 
-  // Siapkan daftar halaman yang akan ditampilkan
   final List<Widget> _pages = const [
-    HomeScreen(),    // Halaman di index 0
-    AddHabitPage(),  // Halaman di index 1
-    ProfileScreen(), // Halaman di index 2
+    HomeScreen(),
+    AddHabitPage(),
+    ProfileScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Body sekarang akan secara dinamis menampilkan halaman dari list _pages
-      // berdasarkan _pageIndex yang aktif.
       body: _pages[_pageIndex],
 
-      // Ini adalah SATU-SATUNYA CurvedNavigationBar di aplikasi Anda.
       bottomNavigationBar: CurvedNavigationBar(
-        // Gunakan transparent agar body di belakang kurva bisa terlihat
         backgroundColor: Colors.transparent, 
         color: Colors.deepPurple,
         animationDuration: const Duration(milliseconds: 300),
-        index: _pageIndex, // Bind index navbar dengan state
+        index: _pageIndex,
         onTap: (index) {
-          // Ketika item di-tap, kita hanya perlu mengubah state dengan setState
-          // Flutter akan otomatis membangun ulang body dengan halaman yang benar
           setState(() {
             _pageIndex = index;
           });
